32fe10cb3720a7ebdbd01bf3c0badab1cc3fe070
PruthviKausla <pkasula@uncc.edu>
2021-09-19 12:48:19 -0400

    first commit

---
+++ untitled/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static String helloWorld() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(helloWorld());
+    }
+}
